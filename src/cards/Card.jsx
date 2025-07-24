6a4ff03c8d16dfa99e43a2bb16cd1dd582cbd56f
Maxmutov Nuriddin <mahmudovnuriddin35@gmail.com>
2025-07-24 17:04:18 +0500

    create 4 files and update 1 file

--- src/cards/Card.jsx
+++ src/cards/Card.jsx
@@ -5,32 +5,38 @@ const CardProject = () => {
     {
       title: "YouTube",
       img: "/src/assets/photo_2025-07-23_15-45-31.jpg",
-      link: "https://marketing-association-of-uzbekistan.netlify.app/",
+      link: "https://mv-youtube.netlify.app/",
+      git: "https://github.com/maxmutov-nuriddin?tab=repositories",
     },
     {
       title: "MAOU",
       img: "/src/assets/image.png",
       link: "https://marketing-association-of-uzbekistan.netlify.app/",
+      git: "https://github.com/maxmutov-nuriddin/maou-website",
     },
     {
-      title: "MAOU",
-      img: "/src/assets/image.png",
-      link: "https://marketing-association-of-uzbekistan.netlify.app/",
+      title: "Turkish Dishes",
+      img: "/src/assets/dishes.jpg",
+      link: "https://mv-eat-turkish-site.netlify.app/",
+      git: "https://github.com/maxmutov-nuriddin?tab=repositories",
     },
     {
-      title: "MAOU",
-      img: "/src/assets/image.png",
-      link: "https://marketing-association-of-uzbekistan.netlify.app/",
+      title: "Product Site",
+      img: "/src/assets/product.jpg",
+      link: "https://mv-product-site.netlify.app/",
+      git: "https://github.com/maxmutov-nuriddin?tab=repositories",
     },
     {
-      title: "MAOU",
-      img: "/src/assets/image.png",
-      link: "https://marketing-association-of-uzbekistan.netlify.app/",
+      title: "logoipsum",
+      img: "/src/assets/logoipsum.jpg",
+      link: "https://logo-ipsum-site.netlify.app/",
+      git: "https://github.com/maxmutov-nuriddin?tab=repositories",
     },
     {
-      title: "MAOU",
-      img: "/src/assets/image.png",
-      link: "https://marketing-association-of-uzbekistan.netlify.app/",
+      title: "Coffee Roasters",
+      img: "/src/assets/cofferoasters.jpg",
+      link: "https://mv-coffe-site.netlify.app/",
+      git: "https://github.com/maxmutov-nuriddin?tab=repositories",
     },
   ];
 
@@ -43,12 +49,13 @@ const CardProject = () => {
           shadow="sm"
           onPress={() => console.log("item pressed")}
           className="border rounded-xl border-transparent transition duration-500 ease-in-out 
-    hover:scale-105 hover:shadow-2xl hover:shadow-blue-800/50"
+    hover:scale-105 hover:shadow-2xl hover:shadow-[#504B38] "
         >
-          <CardBody className="overflow-visible p-0">
+          <CardBody className="overflow-visible p-0 ">
             <Image
               alt={item.title}
-              className="w-full object-cover h-[140px]"
+              className="w-full object-cover h-[140px] transition duration-500 ease-in-out 
+    hover:scale-105 hover:shadow-2xl hover:shadow-[#504B38]"
               radius="lg"
               shadow="sm"
               src={item.img}
@@ -57,13 +64,22 @@ const CardProject = () => {
           </CardBody>
           <CardFooter className="text-small justify-between bg-[#EBE5C2]">
             <b>{item.title}</b>
-            <a
-              href={item.link}
-              target="_blank"
-              className="text-sm border-[#504B38] rounded-lg bg-[#504B38] text-[#F8F3D9] px-5 py-1 text-center hover:bg-[#B9B28A] hover:text-[#504B38] transition duration-700 ease-in-out"
-            >
-              View
-            </a>
+            <div className="flex gap-2">
+              <a
+                href={item.git}
+                target="_blank"
+                className="text-sm border-[#504B38] rounded-lg bg-[#504B38] text-[#F8F3D9] px-5 py-1 text-center hover:bg-[#B9B28A] hover:text-[#504B38] transition duration-700 ease-in-out"
+              >
+                GitHub
+              </a>
+              <a
+                href={item.link}
+                target="_blank"
+                className="text-sm border-[#504B38] rounded-lg bg-[#504B38] text-[#F8F3D9] px-5 py-1 text-center hover:bg-[#B9B28A] hover:text-[#504B38] transition duration-700 ease-in-out"
+              >
+                View
+              </a>
+            </div>
           </CardFooter>
         </Card>
       ))}
